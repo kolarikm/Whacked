23220fecde13eb52d7b0e08922e68f5116a16597
kolarikm <kolarikm@mail.gvsu.edu>
2017-04-02 13:23:54 -0400

    Threads locking, syncing, unlocking

--- src/Whacked.java
+++ src/Whacked.java
@@ -7,8 +7,9 @@ import java.util.concurrent.TimeUnit;
 import java.util.Random;
 
 public class Whacked {
-    
+
     static Semaphore sem;
+    protected static int[][] holes;
 
     static class Mole extends Thread {
 
@@ -21,6 +22,7 @@ public class Whacked {
 
         public void run() {
             try {
+
                 System.out.println("Mole " + id + " attempting to acquire lock...");
                 sem.acquire();
                 System.out.println("Mole " + id + " acquired lock!");
@@ -30,6 +32,10 @@ public class Whacked {
                     System.out.println("Mole " + id + " is doing work...");
                     time = rand.nextInt((10 - 1))+1;
                     TimeUnit.SECONDS.sleep(time);
+                    synchronized (holes) {
+                        holes[this.id][this.id] = this.id+1;
+                    }
+
                 } finally {
                     System.out.println("Mole " + id + " releasing lock after " + time + " seconds.");
                     sem.release();
@@ -41,13 +47,32 @@ public class Whacked {
         }
     }
 
-    public static void main (String[] args) {
+    private static void print() {
+        for (int i = 0; i < holes.length; i++) {
+            for (int j = 0; j < holes[i].length; j++) {
+                System.out.print("" + holes[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
-        sem = new Semaphore(Integer.parseInt(args[1]));
+    public static void main (String[] args) throws InterruptedException {
+
+        Whacked game = new Whacked();
+
+        try {
+            int dim = Integer.parseInt(args[0]);
+            sem = new Semaphore(dim);
+            holes = new int[dim][dim];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         for (int i = 0; i < Integer.parseInt(args[0]); i++) {
             new Mole(i).start();
         }
-    }
 
+        TimeUnit.SECONDS.sleep(13);
+        print();
+    }
 }
