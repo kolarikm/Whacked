d7d99fb0eb3280de89ef514c775b75dcd97ca5a7
kolarikm <kolarikm@mail.gvsu.edu>
2017-04-03 18:17:10 -0400

    Correct number of threads spawning

--- src/Whacked.java
+++ src/Whacked.java
@@ -1,46 +1,54 @@
-/**s
+/**
  * Created by Andromeda on 4/1/17.
  */
 
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Whacked {
 
     static Semaphore sem;
-    protected static int[][] holes;
+    static volatile int[][] holes;
+    //static AtomicIntegerArray[][] holes;
 
     static class Mole extends Thread {
 
-        int id;
+        int i, j;
         Random rand = new Random();
 
-        Mole(int id) {
-            this.id = id;
+        Mole(int i, int j) {
+            this.i = i;
+            this.j = j;
         }
 
         public void run() {
             try {
-
-                System.out.println("Mole " + id + " attempting to acquire lock...");
-                sem.acquire();
-                System.out.println("Mole " + id + " acquired lock!");
-                int time = 0;
-
-                try {
-                    System.out.println("Mole " + id + " is doing work...");
-                    time = rand.nextInt((10 - 1))+1;
+                for (int x = 3; x > 0; x--) {
+                    //System.out.println("Mole " + id + " attempting to acquire lock...");
+                    int time = rand.nextInt((3-1))+1;
                     TimeUnit.SECONDS.sleep(time);
-                    synchronized (holes) {
-                        holes[this.id][this.id] = this.id+1;
+                    sem.acquire();
+                    //System.out.println("Mole " + id + " acquired lock!");
+                    try {
+                        //System.out.println("Mole " + id + " is doing work...");
+                        time = rand.nextInt((5 - 1))+1;
+                        TimeUnit.SECONDS.sleep(time);
+                        synchronized (holes) {
+                            holes[this.i][this.j] = (i + 1) * (j + 1);
+                            System.out.println("Mole " + i + ", " + j + " popped up!");
+                            print();
+                        }
+                    } finally {
+                        //System.out.println("Mole " + id + " releasing lock after " + time + " seconds.");
+                        sem.release();
+                        synchronized (holes) {
+                            //holes[this.id][this.id] = 0;
+                            System.out.println("Mole " + i + ", " + j + " retreated!");
+                        }
                     }
-
-                } finally {
-                    System.out.println("Mole " + id + " releasing lock after " + time + " seconds.");
-                    sem.release();
                 }
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -48,31 +56,33 @@ public class Whacked {
     }
 
     private static void print() {
-        for (int i = 0; i < holes.length; i++) {
-            for (int j = 0; j < holes[i].length; j++) {
-                System.out.print("" + holes[i][j] + " ");
+        synchronized (holes) {
+            for (int i = 0; i < holes.length; i++) {
+                for (int j = 0; j < holes[i].length; j++) {
+                    System.out.print("" + holes[i][j] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 
     public static void main (String[] args) throws InterruptedException {
 
         Whacked game = new Whacked();
+        int dim = 0;
 
         try {
-            int dim = Integer.parseInt(args[0]);
+            dim = Integer.parseInt(args[0]);
             sem = new Semaphore(dim);
             holes = new int[dim][dim];
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-            new Mole(i).start();
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                new Mole(i, j).start();
+            }
         }
-
-        TimeUnit.SECONDS.sleep(13);
-        print();
     }
 }
