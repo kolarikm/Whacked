2c334fac6a7da5122c524446b3772adbd1285bb8
kolarikm <kolarikm@mail.gvsu.edu>
2017-04-06 10:36:22 -0400

    Moles working, score working

--- src/Panel.java
+++ src/Panel.java
@@ -9,11 +9,16 @@ import javax.swing.*;
 public class Panel extends JFrame implements ActionListener {
 
     public static int dim;
+    public static int score;
+    public static int time = 30;
+    public static JLabel scoreLabel;
+    public static JLabel timeLabel;
+    public static Container pane;
     public static MoleButton[][] moles;
 
     public Panel() {
         setTitle("Whack-a-Mole");
-        Container pane = getContentPane();
+        pane = getContentPane();
 
         String input = JOptionPane.showInputDialog(pane, "Desired board dimension?");
 
@@ -24,22 +29,29 @@ public class Panel extends JFrame implements ActionListener {
             System.exit(1);
         }
 
-        pane.setLayout(new GridLayout(dim, dim));
+        pane.setLayout(new GridLayout(dim+1, dim, 5, 5));
         moles = new MoleButton[dim][dim];
 
         int id = 1;
         for (int i = 0; i < dim; i++) {
             for (int j = 0; j < dim; j++) {
-                MoleButton button = new MoleButton();
+                MoleButton button = new MoleButton(new Tuple<>(i, j));
                 pane.add(button);
                 button.addActionListener(this);
                 moles[i][j] = button;
             }
         }
+
+        scoreLabel = new JLabel("Score: " + score);
+        timeLabel = new JLabel("Time: " + time);
+        pane.add(scoreLabel);
+        pane.add(timeLabel);
     }
 
     public static void whack(Whacked game) throws InterruptedException {
         //Every 1/4 second, check state of the game.board and update all buttons based on that state
+        score = 0;
+        int slept = 0;
         boolean playing = true;
         while (playing) {
             for (int i = 0; i < dim; i++) {
@@ -47,23 +59,63 @@ public class Panel extends JFrame implements ActionListener {
                     int[][] state = game.getBoard();
                     if (state[i][j] == 1) {
                         moles[i][j].up();
+                    } else {
+                        moles[i][j].down();
                     }
                 }
             }
-            TimeUnit.MILLISECONDS.sleep(1000);
+            scoreLabel.setText("Score: " + score);
+            TimeUnit.MILLISECONDS.sleep(250);
+            slept++;
+            if (slept % 4 == 0) {
+                time--;
+                timeLabel.setText("Time: " + time);
+            }
+
+            if (time == 0) {
+                playing = false;
+            }
+        }
+
+        //Game is over, present the score and then exit
+        JOptionPane.showMessageDialog(pane, "Game Over!\nYour score was: " + score);
+        System.exit(0);
+    }
+
+    private class Tuple<X, Y> {
+        public final X x;
+        public final Y y;
+
+        public Tuple(X x, Y y) {
+            this.x = x;
+            this.y = y;
         }
     }
 
     private static class MoleButton extends JButton {
         public boolean up;
-        public MoleButton() {
+        public int x, y;
+
+        public MoleButton(Tuple coords) {
             super();
             this.up = false;
             this.setText("");
+            this.x = (int)coords.x;
+            this.y = (int)coords.y;
         }
+
         public void up() {
             this.up = true;
             this.setText("UP!");
+        }
+
+        public void down() {
+            this.up = false;
+            this.setText("");
+        }
+
+        public boolean check() {
+            return this.up;
         }
     }
 
@@ -82,7 +134,10 @@ public class Panel extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-        JButton b = (JButton)e.getSource();
-        ///Check
+        MoleButton mb = (MoleButton)e.getSource();
+        if (mb.check()) {
+            mb.setText("");
+            score++;
+        }
     }
 }
--- src/Whacked.java
+++ src/Whacked.java
@@ -23,10 +23,11 @@ public class Whacked {
             e.printStackTrace();
         }
 
+        int id = 0;
         for (int i = 0; i < dim; i++) {
             for (int j = 0; j < dim; j++) {
-                new Mole(i, j).start();
-                System.out.println(String.format("Mole @ %1$d, %2$d", i, j));
+                new Mole(i, j, id++);
+                //System.out.println(String.format("Mole @ %1$d, %2$d", i, j));
             }
         }
         this.playing = true;
@@ -43,54 +44,54 @@ public class Whacked {
 
     static class Mole extends Thread {
 
-        int i, j;
+        int i, j, moleId;
         Random rand = new Random();
 
-        Mole(int i, int j) {
+        Mole(int i, int j, int moleId) {
             this.i = i;
             this.j = j;
+            this.moleId = moleId;
+            start();
         }
 
-        public void run () {
+        public int getMoleId() {
+            return this.moleId;
+        }
+
+        public void run ()  {
+            System.out.println(this.getMoleId());
             while (playing) {
-                //Set a random time for the thread to sleep before requesting semaphore
+                try {
+                    //Initially sleep for a random time before requesting semaphore for the first time
+                    TimeUnit.MILLISECONDS.sleep(rand.nextInt(1000));
+                    while (playing) {
+                        try {
+                            sem.acquire();
 
-                //Once semaphore is acquired, set the state of the board for that thread
-                //Sleep between 1 and 5 seconds before retreating
+                            //Once semaphore is acquired, set the state of the board for that thread
+                            //Sleep between 1 and 5 seconds before retreating
+                            synchronized (holes) {
+                                holes[this.i][this.j] = 1;
+                            }
 
-                //Once semaphore has been released, set state of board back to 0
-                //Sleep between 1-3 seconds before requesting semaphore again
-                holes[i][j] = 1;
+                            //Sleep for up to 4 seconds in the on or "up" state
+                            TimeUnit.MILLISECONDS.sleep(rand.nextInt(4000));
+                        } finally {
+                            //Release the semaphore and update the board state for that hole
+                            sem.release();
+                            synchronized (holes) {
+                                holes[this.i][this.j] = 0;
+                            }
+
+                            //Sleep before requesting semaphore once again
+                            TimeUnit.MILLISECONDS.sleep(rand.nextInt(2000));
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
-
-//        public void run() {
-//            try {
-//                for (int x = 10; x > 0; x--) {
-//                    int time = rand.nextInt(300);
-//                    TimeUnit.MILLISECONDS.sleep(time);
-//                    sem.acquire();
-//                    try {
-//                        time = rand.nextInt(600);
-//                        TimeUnit.SECONDS.sleep(time);
-//                        synchronized (holes) {
-//                            holes[this.i][this.j] = 1;
-//                            System.out.println("Setting mole " + this.getName());
-//                        }
-//                    } finally {
-//                        sem.release();
-//                        synchronized (holes) {
-//                            holes[this.i][this.j] = 0;
-//                            System.out.println("Removing mole " + this.getName());
-//                            time = rand.nextInt(150);
-//                            TimeUnit.MILLISECONDS.sleep(time);
-//                        }
-//                    }
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
     }
 
     private static void print() {
@@ -108,9 +109,5 @@ public class Whacked {
 
     public int[][] getBoard() {
         return this.holes;
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-
     }
 }
